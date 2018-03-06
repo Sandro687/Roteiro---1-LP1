933a573dc12c345390c586eb2f2f737f9d45b511
Sandro687 <34144569+Sandro687@users.noreply.github.com>
2018-03-06 18:02:33 -0300

    Atualização de código!

--- Roteiro1-1.c
+++ Roteiro1-1.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main (void){
 
     double salarioB, salarioL, desconto;
 
-    printf("Ditite o valor do salario bruto.\n");
+    printf("Digite o valor do salario bruto.\n");
     scanf ("%lf", &salarioB);
 
     if (salarioB <= 420){
-    desconto = (8/100) * salarioB;
+    desconto = 0.08 * salarioB;
     salarioL = salarioB - desconto;
     }else if (salarioB > 420 && salarioB <= 1350){
-    desconto = (9/100) * salarioB;
+    desconto = 0.09 * salarioB;
     salarioL = salarioB - desconto;
     }else if (salarioB > 1350){
-    desconto = (10/100) * salarioB;
+    desconto = 0.1 * salarioB;
     salarioL = salarioB - desconto;
     }
     printf("Salario liquido = %.2lf. Salario bruto = %.2lf. Desconto = %.2lf.\n", salarioL, salarioB, desconto);
